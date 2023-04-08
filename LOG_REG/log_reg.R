2d9774f21d2968744b8237f6e05324b9b332e554
zwehrspan <zwehrspan@gmail.com>
2023-04-08 16:38:58 -0600

    Initial commit

---
+++ LOG_REG/log_reg.R
@@ -0,0 +1,88 @@
+library(tidyverse)
+library(tidymodels)
+
+set.seed(123)
+fullTrainData <- read.csv("../titanic/train.csv")
+finalTestData <- read.csv("../titanic/test.csv")
+finalTestDataORI <- read.csv("../titanic/test.csv")
+
+pred <-
+    c("Pclass", "Sex", "Age", "SibSp", "Parch", "Embarked", "Fare")
+
+fullTrainData <- fullTrainData %>%
+    mutate(Survived = as.factor(Survived)) %>%
+    mutate(Age = case_when(is.na(Age) ~ mean(Age, na.rm = TRUE),
+                           TRUE ~ Age)) %>%
+    mutate(Embarked = case_when(Embarked == "" ~ "UNKNOWN",
+                                TRUE ~ Embarked)) %>%
+    mutate(Fare = case_when(is.na(Fare) ~ mean(Fare, na.rm = TRUE),
+                            TRUE ~ Fare)) %>%
+    select(c("Survived", all_of(pred)))
+
+finalTestData <- finalTestData %>%
+    mutate(Age = case_when(is.na(Age) ~ mean(Age, na.rm = TRUE),
+                           TRUE ~ Age)) %>%
+    mutate(Embarked = case_when(Embarked == "" ~ "UNKNOWN",
+                                TRUE ~ Embarked)) %>%
+    mutate(Fare = case_when(is.na(Fare) ~ mean(Fare, na.rm =
+                                                   TRUE),
+                            TRUE ~ Fare)) %>%
+    select(c(all_of(pred)))
+
+
+
+# Create a recipe for making dummy vars
+dataRecipe <-
+    recipe(Survived ~ ., data = fullTrainData) %>%
+    step_dummy(all_nominal_predictors()) %>%
+    step_zv(all_predictors())
+
+dataSplit <-
+    initial_split(fullTrainData, prop = 3 / 4,
+                  strata = Survived)
+
+# Create data frames for the two sets:
+trainData <- training(dataSplit)
+testData  <- testing(dataSplit)
+
+
+rfModelLogReg <-
+    logistic_reg(mode = "classification", engine = "glm")
+
+dataWF <-
+    workflow() %>%
+    add_model(rfModelLogReg) %>%
+    add_recipe(dataRecipe)
+
+rfModelLogRegFit <- dataWF %>%
+    fit(data = trainData)
+
+rfTrainingPred <-
+    predict(rfModelLogRegFit, trainData) %>%
+    bind_cols(predict(rfModelLogRegFit, trainData, type = "prob")) %>%
+    bind_cols(trainData %>%
+                  select(Survived))
+
+rfTrainingPred %>%                # training set predictions+roc_auc(truth = Survived, .pred_0)
+    roc_auc(truth = Survived, .pred_0)
+rfTrainingPred %>%                # training set predictions
+    accuracy(truth = Survived, .pred_class)
+#  Testing Data
+rfTestingPred <-
+    predict(rfModelLogRegFit, testData) %>%
+    bind_cols(predict(rfModelLogRegFit, testData, type = "prob")) %>%
+    bind_cols(testData %>%
+                  select(Survived))
+
+rfTestingPred %>%                # testing set predictions
+    roc_auc(truth = Survived, .pred_0)
+
+rfTestingPred %>%                # testing set predictions
+    accuracy(truth = Survived, .pred_class)
+
+
+rfTestingPred <-
+    predict(rfModelLogRegFit, finalTestData)
+finalOutput <- cbind.data.frame(finalTestDataORI$PassengerId, rfTestingPred$.pred_class)
+colnames(finalOutput) <- c("PassengerId", "Survived")
+write_csv(finalOutput, "submission.csv")
